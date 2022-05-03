0d3136d408d2eb301d0cd8abbb8c91ba9f61fb3f
Vilem Zavodny <vilem.zavodny@espressif.com>
2022-05-03 12:28:54 +0200

    bsp: Add esp_lcd_touch component

---
+++ components/esp_lcd_touch/include/esp_lcd_touch_io.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief I2C Touch Device Type
+ *
+ */
+typedef struct {
+    int port;
+} esp_lcd_touch_dev_i2c_t;
+
+
+#ifdef __cplusplus
+}
+#endif
