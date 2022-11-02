9f1200811c8fb4b8b8c18d12b20ce5194b5ddee1
Liu Zhongwei <liuzhongwei@espressif.com>
2022-11-02 20:28:21 +0800

    modify set/get function to operate mutiple IOs
    some error fix

--- components/io_expander/esp_io_expander/esp_io_expander.c
+++ components/io_expander/esp_io_expander/esp_io_expander.c
@@ -13,11 +13,7 @@
 
 #include "esp_io_expander.h"
 
-/* Pin num must be smaller than 32 (bit width of uint32_t) */
-#define PIN_NUM_MAX(handle)                 (((handle)->config.io_amount < IO_EXPANDER_PIN_NUM_MAX) ?  \
-                                             (handle)->config.io_amount : (IO_EXPANDER_PIN_NUM_MAX - 1))
-/* Check whether it is a valid pin number */
-#define IS_VALID_PIN_NUM(pin_num, handle)   ((pin_num) < PIN_NUM_MAX(handle))
+#define VALID_IO_COUNT(handle)      ((handle)->config.io_count <= IO_COUNT_MAX ? (handle)->config.io_count : IO_COUNT_MAX)
 
 /**
  * @brief Register type
@@ -34,11 +30,12 @@ static char *TAG = "io_expander";
 static esp_err_t write_reg(esp_io_expander_handle_t handle, reg_type_t reg, uint32_t value);
 static esp_err_t read_reg(esp_io_expander_handle_t handle, reg_type_t reg, uint32_t *value);
 
-esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, esp_io_expander_dir_t direction)
+esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, uint32_t pin_num_mask, esp_io_expander_dir_t direction)
 {
-    ESP_RETURN_ON_FALSE(
-        IS_VALID_PIN_NUM(pin_num, handle), ESP_ERR_INVALID_ARG,
-        TAG, "Invalid pin_num(%d), max(%d)", pin_num, PIN_NUM_MAX(handle));
+    ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, TAG, "Invalid handle");
+    if (pin_num_mask >= BIT(VALID_IO_COUNT(handle))) {
+        ESP_LOGW(TAG, "Pin num mask out of range, bit higher than %d won't work", VALID_IO_COUNT(handle) - 1);
+    }
 
     bool is_output = (direction == IO_EXPANDER_OUTPUT) ? true : false;
     uint32_t dir_reg, temp;
@@ -47,11 +44,11 @@ esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, esp_io_expand
     if ((is_output && !handle->config.flags.dir_out_bit_zero) || (!is_output && handle->config.flags.dir_out_bit_zero)) {
         /* 1. Output && Set 1 to output */
         /* 2. Input && Set 1 to input */
-        dir_reg |= BIT(pin_num) ;
+        dir_reg |= pin_num_mask;
     } else {
         /* 3. Output && Set 0 to output */
         /* 4. Input && Set 0 to input */
-        dir_reg &= ~BIT(pin_num) ;
+        dir_reg &= ~pin_num_mask;
     }
     /* Write to reg only when different */
     if (dir_reg != temp) {
@@ -61,21 +58,29 @@ esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, esp_io_expand
     return ESP_OK;
 }
 
-esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, uint8_t level)
+esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, uint32_t pin_num_mask, uint8_t level)
 {
-    ESP_RETURN_ON_FALSE(
-        IS_VALID_PIN_NUM(pin_num, handle), ESP_ERR_INVALID_ARG,
-        TAG, "Invalid pin_num(%d), max(%d)", pin_num, PIN_NUM_MAX(handle));
+    ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, TAG, "Invalid handle");
+    if (pin_num_mask >= BIT(VALID_IO_COUNT(handle))) {
+        ESP_LOGW(TAG, "Pin num mask out of range, bit higher than %d won't work", VALID_IO_COUNT(handle) - 1);
+    }
 
     uint32_t dir_reg, dir_bit;
     ESP_RETURN_ON_ERROR(read_reg(handle, REG_DIRECTION, &dir_reg), TAG, "Read direction reg failed");
-    dir_bit = dir_reg & BIT(pin_num);
-    /* Check whether it is in input mode */
-    if ((dir_bit && handle->config.flags.dir_out_bit_zero) || (!dir_bit && !handle->config.flags.dir_out_bit_zero)) {
-        /* 1. 1 && Set 1 to input */
-        /* 2. 0 && Set 0 to input */
-        ESP_LOGE(TAG, "Can't set level in input mode");
-        return ESP_ERR_INVALID_STATE;
+
+    uint8_t io_count = VALID_IO_COUNT(handle);
+    /* Check every target pin's direction, must be in output mode */
+    for (int i = 0; i < io_count; i++) {
+        if (pin_num_mask & BIT(i)) {
+            dir_bit = dir_reg & BIT(i);
+            /* Check whether it is in input mode */
+            if ((dir_bit && handle->config.flags.dir_out_bit_zero) || (!dir_bit && !handle->config.flags.dir_out_bit_zero)) {
+                /* 1. 1 && Set 1 to input */
+                /* 2. 0 && Set 0 to input */
+                ESP_LOGE(TAG, "Pin[%d] can't set level in input mode", i);
+                return ESP_ERR_INVALID_STATE;
+            }
+        }
     }
 
     uint32_t output_reg, temp;
@@ -86,11 +91,11 @@ esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, esp_io_expa
     if ((level && !handle->config.flags.output_high_bit_zero) || (!level && handle->config.flags.output_high_bit_zero)) {
         /* 1. High level && Set 1 to output high */
         /* 2. Low level && Set 1 to output low */
-        output_reg |= BIT(pin_num);
+        output_reg |= pin_num_mask;
     } else {
         /* 3. High level && Set 0 to output high */
         /* 4. Low level && Set 0 to output low */
-        output_reg &= ~BIT(pin_num);
+        output_reg &= ~pin_num_mask;
     }
     /* Write to reg only when different */
     if (output_reg != temp) {
@@ -100,22 +105,22 @@ esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, esp_io_expa
     return ESP_OK;
 }
 
-esp_err_t esp_io_expander_get_level(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, uint8_t *level)
+esp_err_t esp_io_expander_get_level(esp_io_expander_handle_t handle, uint32_t pin_num_mask, uint32_t *level_mask)
 {
-    ESP_RETURN_ON_FALSE(level, ESP_ERR_INVALID_ARG, TAG, "Invalid level");
-    ESP_RETURN_ON_FALSE(
-        IS_VALID_PIN_NUM(pin_num, handle), ESP_ERR_INVALID_ARG,
-        TAG, "Invalid pin_num(%d), max(%d)", pin_num, PIN_NUM_MAX(handle));
+    ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, TAG, "Invalid handle");
+    ESP_RETURN_ON_FALSE(level_mask, ESP_ERR_INVALID_ARG, TAG, "Invalid level");
+    if (pin_num_mask >= BIT(VALID_IO_COUNT(handle))) {
+        ESP_LOGW(TAG, "Pin num mask out of range, bit higher than %d won't work", VALID_IO_COUNT(handle) - 1);
+    }
 
-    uint32_t input_reg, level_bit;
+    uint32_t input_reg;
     ESP_RETURN_ON_ERROR(read_reg(handle, REG_INPUT, &input_reg), TAG, "Read input reg failed");
-    level_bit = input_reg & BIT(pin_num);
     if (!handle->config.flags.input_high_bit_zero) {
         /* Get 1 when input high level */
-        *level = (level_bit) ? true : false;
+        *level_mask = input_reg & pin_num_mask;
     } else {
         /* Get 0 when input high level */
-        *level = (!level_bit) ? true : false;
+        *level_mask = ~input_reg & pin_num_mask;
     }
 
     return ESP_OK;
@@ -125,7 +130,7 @@ esp_err_t esp_io_expander_print_state(esp_io_expander_handle_t handle)
 {
     ESP_RETURN_ON_FALSE(handle, ESP_ERR_INVALID_ARG, TAG, "Invalid handle");
 
-    uint8_t io_amount = handle->config.io_amount;
+    uint8_t io_count = VALID_IO_COUNT(handle);
     uint32_t input_reg, output_reg, dir_reg;
     ESP_RETURN_ON_ERROR(read_reg(handle, REG_INPUT, &input_reg), TAG, "Read input reg failed");
     ESP_RETURN_ON_ERROR(read_reg(handle, REG_OUTPUT, &output_reg), TAG, "Read output reg failed");
@@ -143,7 +148,7 @@ esp_err_t esp_io_expander_print_state(esp_io_expander_handle_t handle)
         dir_reg ^= 0xffffffff;
     }
 
-    for (int i = 0; i < io_amount; i++) {
+    for (int i = 0; i < io_count; i++) {
         ESP_LOGI(TAG, "Index[%d] | Dir[%s] | In[%d] | Out[%d]", i, (dir_reg & BIT(i)) ? "Out" : "In",
                  (input_reg & BIT(i)) ? 1 : 0, (output_reg & BIT(i)) ? 1 : 0);
     }
--- components/io_expander/esp_io_expander/include/esp_io_expander.h
+++ components/io_expander/esp_io_expander/include/esp_io_expander.h
@@ -15,6 +15,8 @@
 extern "C" {
 #endif
 
+#define IO_COUNT_MAX        (sizeof(uint32_t) * 8)
+
 /**
  * @brief IO Expander Device Type
  *
@@ -27,35 +29,38 @@ typedef esp_io_expander_t *esp_io_expander_handle_t;
  *
  */
 typedef enum {
-    IO_EXPANDER_PIN_NUM_0  = 0,
-    IO_EXPANDER_PIN_NUM_1  = 1,
-    IO_EXPANDER_PIN_NUM_2  = 2,
-    IO_EXPANDER_PIN_NUM_3  = 3,
-    IO_EXPANDER_PIN_NUM_4  = 4,
-    IO_EXPANDER_PIN_NUM_5  = 5,
-    IO_EXPANDER_PIN_NUM_6  = 6,
-    IO_EXPANDER_PIN_NUM_7  = 7,
-    IO_EXPANDER_PIN_NUM_8  = 8,
-    IO_EXPANDER_PIN_NUM_9  = 9,
-    IO_EXPANDER_PIN_NUM_10 = 10,
-    IO_EXPANDER_PIN_NUM_11 = 11,
-    IO_EXPANDER_PIN_NUM_12 = 12,
-    IO_EXPANDER_PIN_NUM_13 = 13,
-    IO_EXPANDER_PIN_NUM_14 = 14,
-    IO_EXPANDER_PIN_NUM_15 = 15,
-    IO_EXPANDER_PIN_NUM_16 = 16,
-    IO_EXPANDER_PIN_NUM_17 = 17,
-    IO_EXPANDER_PIN_NUM_18 = 18,
-    IO_EXPANDER_PIN_NUM_19 = 19,
-    IO_EXPANDER_PIN_NUM_20 = 20,
-    IO_EXPANDER_PIN_NUM_21 = 21,
-    IO_EXPANDER_PIN_NUM_26 = 26,
-    IO_EXPANDER_PIN_NUM_27 = 27,
-    IO_EXPANDER_PIN_NUM_28 = 28,
-    IO_EXPANDER_PIN_NUM_29 = 29,
-    IO_EXPANDER_PIN_NUM_30 = 30,
-    IO_EXPANDER_PIN_NUM_31 = 31,
-    IO_EXPANDER_PIN_NUM_MAX,
+    IO_EXPANDER_PIN_NUM_0  = (1ULL << 0),
+    IO_EXPANDER_PIN_NUM_1  = (1ULL << 1),
+    IO_EXPANDER_PIN_NUM_2  = (1ULL << 2),
+    IO_EXPANDER_PIN_NUM_3  = (1ULL << 3),
+    IO_EXPANDER_PIN_NUM_4  = (1ULL << 4),
+    IO_EXPANDER_PIN_NUM_5  = (1ULL << 5),
+    IO_EXPANDER_PIN_NUM_6  = (1ULL << 6),
+    IO_EXPANDER_PIN_NUM_7  = (1ULL << 7),
+    IO_EXPANDER_PIN_NUM_8  = (1ULL << 8),
+    IO_EXPANDER_PIN_NUM_9  = (1ULL << 9),
+    IO_EXPANDER_PIN_NUM_10 = (1ULL << 10),
+    IO_EXPANDER_PIN_NUM_11 = (1ULL << 11),
+    IO_EXPANDER_PIN_NUM_12 = (1ULL << 12),
+    IO_EXPANDER_PIN_NUM_13 = (1ULL << 13),
+    IO_EXPANDER_PIN_NUM_14 = (1ULL << 14),
+    IO_EXPANDER_PIN_NUM_15 = (1ULL << 15),
+    IO_EXPANDER_PIN_NUM_16 = (1ULL << 16),
+    IO_EXPANDER_PIN_NUM_17 = (1ULL << 17),
+    IO_EXPANDER_PIN_NUM_18 = (1ULL << 18),
+    IO_EXPANDER_PIN_NUM_19 = (1ULL << 19),
+    IO_EXPANDER_PIN_NUM_20 = (1ULL << 20),
+    IO_EXPANDER_PIN_NUM_21 = (1ULL << 21),
+    IO_EXPANDER_PIN_NUM_22 = (1ULL << 22),
+    IO_EXPANDER_PIN_NUM_23 = (1ULL << 23),
+    IO_EXPANDER_PIN_NUM_24 = (1ULL << 24),
+    IO_EXPANDER_PIN_NUM_25 = (1ULL << 25),
+    IO_EXPANDER_PIN_NUM_26 = (1ULL << 26),
+    IO_EXPANDER_PIN_NUM_27 = (1ULL << 27),
+    IO_EXPANDER_PIN_NUM_28 = (1ULL << 28),
+    IO_EXPANDER_PIN_NUM_29 = (1ULL << 29),
+    IO_EXPANDER_PIN_NUM_30 = (1ULL << 30),
+    IO_EXPANDER_PIN_NUM_31 = (1ULL << 31),
 } esp_io_expander_pin_num_t;
 
 /**
@@ -72,7 +77,7 @@ typedef enum {
  *
  */
 typedef struct {
-    uint8_t io_count;                      /*!< Count of device's IO, must be less or equal than `IO_EXPANDER_PIN_NUM_MAX` */
+    uint8_t io_count;                       /*!< Count of device's IO, must be less or equal than `IO_COUNT_MAX` */
     struct {
         uint8_t dir_out_bit_zero : 1;       /*!< If the direction of IO is output, the corresponding bit of the direction register is 0 */
         uint8_t input_high_bit_zero : 1;    /*!< If the input level of IO is high, the corresponding bit of the input register is 0 */
@@ -184,47 +189,47 @@ struct esp_io_expander_s {
 };
 
 /**
- * @brief Set the direction of a specific IO
+ * @brief Set the direction of a set of target IOs
  *
  * @param handle: IO Exapnder handle
- * @param pin_num: IO num, valid from 0 to (`io_amount` - 1)
+ * @param pin_num_mask: Bitwise OR of allowed pin num with type of `esp_io_expander_pin_num_t`
  * @param direction: IO direction (only support input or output now)
  *
  * @return
  *      - ESP_OK: Success, otherwise returns ESP_ERR_xxx
  */
-esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, esp_io_expander_dir_t direction);
+esp_err_t esp_io_expander_set_dir(esp_io_expander_handle_t handle, uint32_t pin_num_mask, esp_io_expander_dir_t direction);
 
 /**
- * @brief Set the output level of a specific IO
+ * @brief Set the output level of a set of target IOs
  *
- * @note The IO must be in output mode first, otherwise this function will return the error `ESP_ERR_INVALID_STATE`
+ * @note All target IOs must be in output mode first, otherwise this function will return the error `ESP_ERR_INVALID_STATE`
  *
  * @param handle: IO Exapnder handle
- * @param pin_num: IO num, valid from 0 to (`io_amount` - 1)
+ * @param pin_num_mask: Bitwise OR of allowed pin num with type of `esp_io_expander_pin_num_t`
  * @param level: 0 - Low level, 1 - High level
  *
  * @return
  *      - ESP_OK: Success, otherwise returns ESP_ERR_xxx
  */
-esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, uint8_t level);
+esp_err_t esp_io_expander_set_level(esp_io_expander_handle_t handle, uint32_t pin_num_mask, uint8_t level);
 
 /**
- * @brief Get the intput level of a specific IO
+ * @brief Get the intput level of a set of target IOs
  *
- * @note This function can be called when the IO is in input mode or output mode
+ * @note This function can be called whenever target IOs are in input mode or output mode
  *
  * @param handle: IO Exapnder handle
- * @param pin_num: IO num, valid from 0 to (`io_amount` - 1)
- * @param level: 0 - Low level, 1 - High level
+ * @param pin_num_mask: Bitwise OR of allowed pin num with type of `esp_io_expander_pin_num_t`
+ * @param level: Bitwise OR of levels. For each bit, 0 - Low level, 1 - High level
  *
  * @return
  *      - ESP_OK: Success, otherwise returns ESP_ERR_xxx
  */
-esp_err_t esp_io_expander_get_level(esp_io_expander_handle_t handle, esp_io_expander_pin_num_t pin_num, uint8_t *level);
+esp_err_t esp_io_expander_get_level(esp_io_expander_handle_t handle, uint32_t pin_num_mask, uint32_t *level_mask);
 
 /**
- * @brief Print the current status of each IO of the device, including direction, input level and output level.
+ * @brief Print the current status of each IO of the device, including direction, input level and output level
  *
  * @param handle: IO Exapnder handle
  *
