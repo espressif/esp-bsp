be1981e183b8c782efb6a15d3bb2acb6f5a7adcc
AndrewCapon <andrew.capon@zen.co.uk>
2023-05-02 09:38:57 +0100

    Fix touch interrupt callbacks .intr_type was being set on the RST pin not the IRQ pin.

--- components/lcd_touch/esp_lcd_touch_cst816s/esp_lcd_touch_cst816s.c
+++ components/lcd_touch/esp_lcd_touch_cst816s/esp_lcd_touch_cst816s.c
@@ -60,6 +60,7 @@ esp_err_t esp_lcd_touch_new_i2c_cst816s(const esp_lcd_panel_io_handle_t io, cons
     if (cst816s->config.int_gpio_num != GPIO_NUM_NC) {
         const gpio_config_t int_gpio_config = {
             .mode = GPIO_MODE_INPUT,
+            .intr_type = GPIO_INTR_NEGEDGE,
             .pin_bit_mask = BIT64(cst816s->config.int_gpio_num)
         };
         ESP_GOTO_ON_ERROR(gpio_config(&int_gpio_config), err, TAG, "GPIO intr config failed");
@@ -68,7 +69,6 @@ esp_err_t esp_lcd_touch_new_i2c_cst816s(const esp_lcd_panel_io_handle_t io, cons
     if (cst816s->config.rst_gpio_num != GPIO_NUM_NC) {
         const gpio_config_t rst_gpio_config = {
             .mode = GPIO_MODE_OUTPUT,
-            .intr_type = GPIO_INTR_NEGEDGE,
             .pin_bit_mask = BIT64(cst816s->config.rst_gpio_num)
         };
         ESP_GOTO_ON_ERROR(gpio_config(&rst_gpio_config), err, TAG, "GPIO reset config failed");
