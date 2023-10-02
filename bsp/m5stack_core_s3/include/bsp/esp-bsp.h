55d2c06f3c4822a6b3f18fdd196caf83e35113a6
Vilem Zavodny <vilem.zavodny@espressif.com>
2023-10-02 16:08:52 +0200

    bsp: Add M5Stack CoreS3

---
+++ bsp/m5stack_core_s3/include/bsp/esp-bsp.h
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+#include "bsp/m5stack_core_s3.h"
--- examples/bsp_ext.py
+++ examples/bsp_ext.py
@@ -58,6 +58,7 @@ def action_extensions(base_actions, project_path=os.getcwd()):
             'esp_bsp_generic',
             'esp32_s3_korvo_1',
             'esp32_p4_function_ev_board',
+            'm5stack_core_s3'
         }
 
         if bsp == '':
