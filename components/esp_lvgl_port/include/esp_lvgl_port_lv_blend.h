a30144632f0a08c6dfb5faaa7ffb7c9bcd8017d5
dailei <dailei@espressif.com>
2024-10-15 12:01:08 +0800

    feature(lvgl_port): Initial support for ppa rendering in lvgl
          Mainly supports the following features:
           - color blend (simple fill)
           - color blend with opa
           - blend normal (blend with argb8888)
           - blend normal (color convert / memcpy)

--- components/esp_lvgl_port/include/esp_lvgl_port_lv_blend.h
+++ components/esp_lvgl_port/include/esp_lvgl_port_lv_blend.h
@@ -18,6 +18,8 @@ extern "C" {
 #warning "esp_lvgl_port_lv_blend.h included, but CONFIG_LV_DRAW_SW_ASM_CUSTOM not set. Assembly rendering not used"
 #else
 
+#include "lv_version.h"
+
 /*********************
  *      DEFINES
  *********************/
@@ -49,20 +51,80 @@ typedef struct {
     uint32_t mask_stride;
 } asm_dsc_t;
 
+#if ((LVGL_VERSION_MAJOR == 9) && (LVGL_VERSION_MINOR == 1))
+
+typedef struct {
+    void * dest_buf;
+    int32_t dest_w;
+    int32_t dest_h;
+    int32_t dest_stride;
+    const lv_opa_t * mask_buf;
+    int32_t mask_stride;
+    lv_color_t color;
+    lv_opa_t opa;
+} bsp_blend_fill_dsc_t;
+
+typedef struct {
+    void * dest_buf;
+    int32_t dest_w;
+    int32_t dest_h;
+    int32_t dest_stride;
+    const lv_opa_t * mask_buf;
+    int32_t mask_stride;
+    const void * src_buf;
+    int32_t src_stride;
+    lv_color_format_t src_color_format;
+    lv_opa_t opa;
+    lv_blend_mode_t blend_mode;
+} bsp_blend_image_dsc_t;
+
+#elif ((LVGL_VERSION_MAJOR == 9) && (LVGL_VERSION_MINOR == 2))
+
+typedef struct {
+    void * dest_buf;
+    int32_t dest_w;
+    int32_t dest_h;
+    int32_t dest_stride;
+    const lv_opa_t * mask_buf;
+    int32_t mask_stride;
+    lv_color_t color;
+    lv_opa_t opa;
+    lv_area_t relative_area;
+} bsp_blend_fill_dsc_t;
+
+typedef struct {
+    void * dest_buf;
+    int32_t dest_w;
+    int32_t dest_h;
+    int32_t dest_stride;
+    const lv_opa_t * mask_buf;
+    int32_t mask_stride;
+    const void * src_buf;
+    int32_t src_stride;
+    lv_color_format_t src_color_format;
+    lv_opa_t opa;
+    lv_blend_mode_t blend_mode;
+    lv_area_t relative_area;    /**< The blend area relative to the layer's buffer area. */
+    lv_area_t src_area;             /**< The original src area. */
+} bsp_blend_image_dsc_t;
+
+#endif
+
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
 extern int lv_color_blend_to_argb8888_esp(asm_dsc_t *asm_dsc);
 
-static inline lv_result_t _lv_color_blend_to_argb8888_esp(_lv_draw_sw_blend_fill_dsc_t *dsc)
+static inline lv_result_t _lv_color_blend_to_argb8888_esp(void *dsc)
 {
+    bsp_blend_fill_dsc_t * fill_dsc = (bsp_blend_fill_dsc_t *) dsc;
     asm_dsc_t asm_dsc = {
-        .dst_buf = dsc->dest_buf,
-        .dst_w = dsc->dest_w,
-        .dst_h = dsc->dest_h,
-        .dst_stride = dsc->dest_stride,
-        .src_buf = &dsc->color,
+        .dst_buf = fill_dsc->dest_buf,
+        .dst_w = fill_dsc->dest_w,
+        .dst_h = fill_dsc->dest_h,
+        .dst_stride = fill_dsc->dest_stride,
+        .src_buf = &fill_dsc->color,
     };
 
     return lv_color_blend_to_argb8888_esp(&asm_dsc);
@@ -70,14 +132,15 @@ static inline lv_result_t _lv_color_blend_to_argb8888_esp(_lv_draw_sw_blend_fill
 
 extern int lv_color_blend_to_rgb565_esp(asm_dsc_t *asm_dsc);
 
-static inline lv_result_t _lv_color_blend_to_rgb565_esp(_lv_draw_sw_blend_fill_dsc_t *dsc)
+static inline lv_result_t _lv_color_blend_to_rgb565_esp(void *dsc)
 {
+    bsp_blend_fill_dsc_t * fill_dsc = (bsp_blend_fill_dsc_t *) dsc;
     asm_dsc_t asm_dsc = {
-        .dst_buf = dsc->dest_buf,
-        .dst_w = dsc->dest_w,
-        .dst_h = dsc->dest_h,
-        .dst_stride = dsc->dest_stride,
-        .src_buf = &dsc->color,
+        .dst_buf = fill_dsc->dest_buf,
+        .dst_w = fill_dsc->dest_w,
+        .dst_h = fill_dsc->dest_h,
+        .dst_stride = fill_dsc->dest_stride,
+        .src_buf = &fill_dsc->color,
     };
 
     return lv_color_blend_to_rgb565_esp(&asm_dsc);
