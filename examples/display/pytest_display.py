70f41c7f87481e8d7c178d8437763b4d3cc3234b
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-12 15:51:59 +0100

    pytest params

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -1,12 +1,17 @@
-# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
-# SPDX-License-Identifier: Unlicense OR CC0-1.0
-from __future__ import unicode_literals
+# SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: CC0-1.0
 
 import pytest
 from pytest_embedded import Dut
 
 
-@pytest.mark.generic
+@pytest.mark.parametrize(
+    'board,port',
+    [
+        pytest.param('esp-box-3', '/dev/boards/esp-box-3', id='esp-box-3'),
+    ],
+    indirect=True,
+)
 def test_display_example(dut: Dut) -> None:
     dut.expect_exact('Add LCD screen')
     dut.expect_exact('Display LVGL animation')
