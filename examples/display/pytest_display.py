f92ba2221a6e35eda885698afc7a52faa0f1907c
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-13 09:35:34 +0100

    params

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -13,9 +13,9 @@ def build_dir(board: str) -> str:
 
 @pytest.mark.esp_box_3
 @pytest.mark.parametrize(
-    'board,port',
+    'flash_port,board',
     [
-        pytest.param('esp-box-3', '/dev/boards/esp-box-3', id='esp-box-3'),
+        pytest.param('/dev/boards/esp-box-3', 'esp-box-3', id='esp-box-3'),
     ],
     indirect=True,
 )
