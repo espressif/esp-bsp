83650fe404d11f16074b1fe2fe989f217b0d48cd
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-17 15:18:48 +0100

    no message

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -6,11 +6,17 @@ from pytest_embedded import Dut
 
 
 @pytest.mark.esp_box_3
-@pytest.mark.esp32_p4_function_ev_board
 @pytest.mark.parametrize(
     'target,port,flash_port',
     [
         pytest.param('esp32s3','/dev/ttyACM7', '/dev/ttyACM7', id='esp-box-3'),
+    ],
+    indirect=True,
+)
+@pytest.mark.esp32_p4_function_ev_board
+@pytest.mark.parametrize(
+    'target,port,flash_port',
+    [
         pytest.param('esp32p4','/dev/ttyUSB4', '/dev/ttyUSB4', id='esp32_p4_function_ev_board'),
     ],
     indirect=True,
