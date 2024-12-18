46d0638fa87063bfa2715c4ba871d9edc4564c58
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-18 10:30:12 +0100

    two boards

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -5,13 +5,13 @@ import pytest
 from pytest_embedded import Dut
 
 
-# @pytest.mark.esp_box_3
+@pytest.mark.esp_box_3
 # @pytest.mark.esp32_p4_function_ev_board
 @pytest.mark.esp32_c3_lcdkit
 @pytest.mark.parametrize(
     'port,flash_port',
     [
-        # pytest.param('/dev/boards/esp-box-3', '/dev/boards/esp-box-3', id='esp_box_3'),
+        pytest.param('/dev/boards/esp-box-3', '/dev/boards/esp-box-3', id='esp_box_3'),
         pytest.param('/dev/boards/esp32_c3_lcdkit', '/dev/boards/esp32_c3_lcdkit', id='esp32_c3_lcdkit'),
         # pytest.param('/dev/boards/esp32_p4_function_ev_board', '/dev/boards/esp32_p4_function_ev_board', id='esp32_p4_function_ev_board'),
     ],
@@ -19,5 +19,4 @@ from pytest_embedded import Dut
 )
 def test_display_example(dut: Dut) -> None:
     dut.expect_exact('example: Display LVGL animation')
-    dut.expect_exact('ESP-BOX-3: Setting LCD backlight: 100%')
     dut.expect_exact('main_task: Returned from app_main()')
