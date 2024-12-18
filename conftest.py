3141372a68668080c068a04b9c4a50cda19e9089
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-18 11:38:35 +0100

    Check marker with params and test ID

---
+++ conftest.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+
+
+# Check marker with params and test ID
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        marker_option = config.getoption("-m")
+        if marker_option not in item.nodeid:
+            item.add_marker(pytest.mark.skip(reason="Not for selected params"))
