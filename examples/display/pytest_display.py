a29051baf19ebd9ad039e02875ce8449a47650fd
Vilem Zavodny <vilem.zavodny@espressif.com>
2024-12-18 10:10:18 +0100

    C3 LCDkit

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -6,12 +6,14 @@ from pytest_embedded import Dut
 
 
 # @pytest.mark.esp_box_3
-@pytest.mark.esp32_p4_function_ev_board
+# @pytest.mark.esp32_p4_function_ev_board
+@pytest.mark.esp32_c3_lcdkit
 @pytest.mark.parametrize(
     'port,flash_port',
     [
         # pytest.param('/dev/boards/esp-box-3', '/dev/boards/esp-box-3', id='esp_box_3'),
-        pytest.param('/dev/boards/esp32_p4_function_ev_board', '/dev/boards/esp32_p4_function_ev_board', id='esp32_p4_function_ev_board'),
+        pytest.param('/dev/boards/esp32_c3_lcdkit', '/dev/boards/esp32_c3_lcdkit', id='esp32_c3_lcdkit'),
+        # pytest.param('/dev/boards/esp32_p4_function_ev_board', '/dev/boards/esp32_p4_function_ev_board', id='esp32_p4_function_ev_board'),
     ],
     indirect=True,
 )
