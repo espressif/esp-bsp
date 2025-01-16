espressif/esp-bsp

commit 91ad61a6a1f5a6ad312b20defbb6060057130e64

Fix(esp_lcd_touch_cst816s): Missing field initializers under C++ (BSP-609) (#470)
* Fix CST816S field order and missing initializers

* Bump CST816S version due to changes in 1c37266

components/lcd_touch/esp_lcd_touch_cst816s/idf_component.yml
@@ -1,4 +1,4 @@
-version: "1.0.3~1"
+version: "1.0.4"
 description: ESP LCD Touch CST816S - touch controller CST816S
 url: https://github.com/espressif/esp-bsp/tree/master/components/lcd_touch/esp_lcd_touch_cst816s
 dependencies:

components/lcd_touch/esp_lcd_touch_cst816s/include/esp_lcd_touch_cst816s.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -41,15 +41,20 @@ esp_err_t esp_lcd_touch_new_i2c_cst816s(const esp_lcd_panel_io_handle_t io, cons
  *
  */
 #define ESP_LCD_TOUCH_IO_I2C_CST816S_CONFIG()             \
-    {                                                    \
+    {                                                     \
         .dev_addr = ESP_LCD_TOUCH_IO_I2C_CST816S_ADDRESS, \
-        .control_phase_bytes = 1,                        \
-        .dc_bit_offset = 0,                              \
-        .lcd_cmd_bits = 8,                              \
-        .flags =                                         \
-        {                                                \
-            .disable_control_phase = 1,                  \
-        }                                                \
+        .on_color_trans_done = 0,                         \
+        .user_ctx = 0,                                    \
+        .control_phase_bytes = 1,                         \
+        .dc_bit_offset = 0,                               \
+        .lcd_cmd_bits = 8,                                \
+        .lcd_param_bits = 0,                              \
+        .flags =                                          \
+        {                                                 \
+            .dc_low_on_data = 0,                          \
+            .disable_control_phase = 1,                   \
+        },                                                \
+        .scl_speed_hz = 0                                 \
     }
 
 #ifdef __cplusplus

commit af490e560b850d75dcf9a86e2dd98bca05425796

fix(cst816s): Provide 100kHz default value for SCL
Bug introduced in https://github.com/espressif/esp-bsp/pull/470
Closes https://github.com/espressif/esp-bsp/issues/485

components/lcd_touch/esp_lcd_touch_cst816s/idf_component.yml
@@ -1,4 +1,4 @@
-version: "1.0.5"
+version: "1.0.6"
 description: ESP LCD Touch CST816S - touch controller CST816S
 url: https://github.com/espressif/esp-bsp/tree/master/components/lcd_touch/esp_lcd_touch_cst816s
 dependencies:

components/lcd_touch/esp_lcd_touch_cst816s/include/esp_lcd_touch_cst816s.h
@@ -72,7 +72,7 @@ esp_err_t esp_lcd_touch_new_i2c_cst816s(const esp_lcd_panel_io_handle_t io, cons
             .dc_low_on_data = 0,                          \
             .disable_control_phase = 1,                   \
         },                                                \
-        .scl_speed_hz = 0                                 \
+        .scl_speed_hz = 100000                            \
     }
 #endif
 