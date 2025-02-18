225367d55f7acfdb4060f913334a6669c9c23144
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-02-18 13:51:01 +0100

    ci(runner): Add benchmark example and print benchmark results

---
+++ examples/display_lvgl_benchmark/main/main.c
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "lv_demos.h"
+#include "bsp/esp-bsp.h"
+
+static char *TAG = "app_main";
+
+#define LOG_MEM_INFO    (0)
+
+void app_main(void)
+{
+    /* Initialize display and LVGL */
+#if defined(BSP_LCD_SUB_BOARD_2_H_RES)
+    /* Only for esp32_s3_lcd_ev_board */
+    bsp_display_cfg_t cfg = {
+        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+    };
+    cfg.lvgl_port_cfg.task_stack = 10000;
+    bsp_display_start_with_config(&cfg);
+#else
+    bsp_display_start();
+#endif
+
+    /* Set display brightness to 100% */
+    bsp_display_backlight_on();
+
+    ESP_LOGI(TAG, "Display LVGL demo");
+    bsp_display_lock(0);
+    lv_demo_benchmark();    /* A demo to measure the performance of LVGL or to compare different settings. */
+    bsp_display_unlock();
+}
