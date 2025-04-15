bf7700c2c274b44175c06df6470a7f42328bf274
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-04-15 15:18:19 +0200

    ci(runner): Process runner in parallel

--- examples/generic_button_led/pytest_generic_button_led.py
+++ examples/generic_button_led/pytest_generic_button_led.py
@@ -7,6 +7,6 @@ from pytest_embedded import Dut
 
 @pytest.mark.esp_bsp_devkit
 @pytest.mark.esp_bsp_generic
-def test_generic_button_led(dut: Dut) -> None:
+def test_example_generic_button_led(dut: Dut) -> None:
     dut.expect_exact('led_indicator: Indicator create successfully.')
     dut.expect_exact('main_task: Returned from app_main()')
