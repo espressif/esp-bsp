fde88f5d81d34ad4975dd18ff62cbe47a32fe896
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-06-24 10:21:33 +0200

    fix(benchmark_example): Fixed stack size for benchmark for esp32_s3_eye board.

--- examples/display_lvgl_benchmark/main/main.c
+++ examples/display_lvgl_benchmark/main/main.c
@@ -24,12 +24,36 @@ static char *TAG = "app_main";
 void app_main(void)
 {
     /* Initialize display and LVGL */
-#if defined(BSP_LCD_SUB_BOARD_2_H_RES)
+
+#if defined(BSP_BOARD_ESP32_S3_LCD_EV_BOARD)
     /* Only for esp32_s3_lcd_ev_board */
     bsp_display_cfg_t cfg = {
         .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
     };
     cfg.lvgl_port_cfg.task_stack = 10000;
+    bsp_display_start_with_config(&cfg);
+#elif defined(BSP_BOARD_ESP32_S3_EYE)
+    /* Only for esp32_s3_eye */
+    bsp_display_cfg_t cfg = {
+        .lvgl_port_cfg = {
+            .task_priority = CONFIG_BSP_DISPLAY_LVGL_TASK_PRIORITY,
+            .task_stack = 10000,
+            .task_affinity = 1,
+            .timer_period_ms = CONFIG_BSP_DISPLAY_LVGL_TICK,
+            .task_max_sleep_ms = CONFIG_BSP_DISPLAY_LVGL_MAX_SLEEP,
+        },
+        .buffer_size = BSP_LCD_DRAW_BUFF_SIZE,
+        .double_buffer = BSP_LCD_DRAW_BUFF_DOUBLE,
+        .flags = {
+#if (CONFIG_BSP_DISPLAY_LVGL_BUFFER_IN_PSRAM && CONFIG_SPIRAM)
+            .buff_dma = false,
+            .buff_spiram = true,
+#else
+            .buff_dma = true,
+            .buff_spiram = false,
+#endif
+        }
+    };
     bsp_display_start_with_config(&cfg);
 #else
     bsp_display_start();
