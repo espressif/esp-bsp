01af2dd64c5e7470bc06f5b12ba2599a4b78c23d
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-09-05 14:56:57 +0200

    bsp(esp32_p4_eye): Board Support Package for ESP32-P4-EYE (generated)

---
+++ bsp/esp32_p4_eye/include/bsp/esp32_p4_eye.h
@@ -0,0 +1,624 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ESP BSP: esp32_p4_eye
+ */
+
+#pragma once
+
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "bsp/config.h"
+#include "driver/i2s_std.h"
+#include "driver/i2c_master.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "esp_vfs_fat.h"
+#include "iot_button.h"
+#include "bsp/display.h"
+#include "led_indicator.h"
+
+
+#if (BSP_CONFIG_NO_GRAPHIC_LIB == 0)
+#include "lvgl.h"
+#include "esp_lvgl_port.h"
+#endif // BSP_CONFIG_NO_GRAPHIC_LIB == 0
+/**************************************************************************************************
+ *  BSP Board Name
+ **************************************************************************************************/
+
+/** @defgroup boardname Board Name
+ *  @brief BSP Board Name
+ *  @{
+ */
+#define BSP_BOARD_ESP32_P4_EYE
+/** @} */ // end of boardname
+
+/**************************************************************************************************
+ *  BSP Capabilities
+ **************************************************************************************************/
+
+/** @defgroup capabilities Capabilities
+ *  @brief BSP Capabilities
+ *  @{
+ */
+#define BSP_CAPS_DISPLAY          1
+#define BSP_CAPS_TOUCH            0
+#define BSP_CAPS_BUTTONS          1
+#define BSP_CAPS_KNOB             1
+#define BSP_CAPS_AUDIO            0
+#define BSP_CAPS_AUDIO_SPEAKER    0
+#define BSP_CAPS_AUDIO_MIC        0
+#define BSP_CAPS_SDCARD           1
+#define BSP_CAPS_LED              1
+#define BSP_CAPS_CAMERA           1
+#define BSP_CAPS_BAT              0
+#define BSP_CAPS_IMU              0
+/** @} */ // end of capabilities
+
+/**************************************************************************************************
+ *  Board pinout
+ **************************************************************************************************/
+
+/** @defgroup g01_i2c I2C
+ *  @brief I2C BSP API
+ *  @{
+ */
+#define BSP_I2C_SCL     (GPIO_NUM_13)
+#define BSP_I2C_SDA     (GPIO_NUM_14)
+/** @} */ // end of i2c
+
+/** @defgroup g04_display Display and Touch
+ *  @brief Display BSP API
+ *  @{
+ */
+#define BSP_LCD_PCLK         (GPIO_NUM_17)
+#define BSP_LCD_DATA0        (GPIO_NUM_16)
+#define BSP_LCD_DC           (GPIO_NUM_19)
+#define BSP_LCD_CS           (GPIO_NUM_18)
+#define BSP_LCD_RST          (GPIO_NUM_15)
+#define BSP_LCD_BACKLIGHT    (GPIO_NUM_20)
+#define BSP_LCD_EN           (GPIO_NUM_12)
+/** @} */ // end of display
+
+/** @defgroup g08_camera Camera
+ *  @brief Camera BSP API
+ *  @{
+ */
+#define BSP_CAMERA_GPIO_XCLK (GPIO_NUM_11)
+#define BSP_CAMERA_RST       (GPIO_NUM_26)
+#define BSP_CAMERA_EN        (GPIO_NUM_12)
+/** @} */ // end of audio
+/* Encoder */
+#define BSP_ENCODER_A        (GPIO_NUM_48)
+#define BSP_ENCODER_B        (GPIO_NUM_47)
+#define BSP_ENCODER_PRESS    (GPIO_NUM_2)
+
+/** @defgroup g05_buttons Buttons
+ *  @brief Buttons BSP API
+ *  @{
+ */
+#define BSP_BUTTON_1_IO      (GPIO_NUM_35)
+/** @} */ // end of buttons
+
+/** @defgroup g06_led Leds
+ *  @brief Leds BSP API
+ *  @{
+ */
+#define BSP_LED_1_IO         (GPIO_NUM_23)
+/** @} */ // end of usb
+
+/** @defgroup g02_storage SD Card and SPIFFS
+ *  @brief SPIFFS and SD card BSP API
+ *  @{
+ */
+#define BSP_SD_D0            (GPIO_NUM_39)
+#define BSP_SD_D1            (GPIO_NUM_40)
+#define BSP_SD_D2            (GPIO_NUM_41)
+#define BSP_SD_D3            (GPIO_NUM_42)
+#define BSP_SD_CMD           (GPIO_NUM_44)
+#define BSP_SD_CLK           (GPIO_NUM_43)
+#define BSP_SD_DET           (GPIO_NUM_45)
+#define BSP_SD_SPI_CLK       (GPIO_NUM_43)
+#define BSP_SD_SPI_MISO      (GPIO_NUM_39)
+#define BSP_SD_SPI_MOSI      (GPIO_NUM_44)
+#define BSP_SD_SPI_CS        (GPIO_NUM_42)
+#define BSP_SD_EN            (GPIO_NUM_46)
+/** @} */ // end of storage
+
+
+/** @defgroup g05_buttons Buttons
+ *  @brief Buttons BSP API
+ *  @{
+ */
+typedef enum {
+    BSP_BUTTON_1,
+    BSP_BUTTON_NUM
+} bsp_button_t;
+/** @} */ // end of buttons
+
+
+/** \addtogroup g06_led
+ *  @brief BSP Leds
+ *  @{
+ */
+typedef enum {
+    BSP_LED_1,
+    BSP_LED_NUM
+} bsp_led_t;
+
+/* Default LED effects */
+enum {
+    BSP_LED_ON,
+    BSP_LED_OFF,
+    BSP_LED_BLINK_FAST,
+    BSP_LED_BLINK_SLOW,
+    BSP_LED_BREATHE_FAST,
+    BSP_LED_BREATHE_SLOW,
+    BSP_LED_MAX,
+};
+/** @} */ // end of leds
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** \addtogroup g01_i2c
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * I2C interface
+ *
+ **************************************************************************************************/
+#define BSP_I2C_NUM     CONFIG_BSP_I2C_NUM
+
+/**
+ * @brief Init I2C driver
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *      - ESP_FAIL              I2C driver installation error
+ *
+ */
+esp_err_t bsp_i2c_init(void);
+
+/**
+ * @brief Deinit I2C driver and free its resources
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *
+ */
+esp_err_t bsp_i2c_deinit(void);
+
+/**
+ * @brief Get I2C driver handle
+ *
+ * @return
+ *      - I2C handle
+ */
+i2c_master_bus_handle_t bsp_i2c_get_handle(void);
+
+/** @} */ // end of i2c
+
+/** \addtogroup g02_storage
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * SPIFFS
+ *
+ * After mounting the SPIFFS, it can be accessed with stdio functions ie.:
+ * \code{.c}
+ * FILE* f = fopen(BSP_SPIFFS_MOUNT_POINT"/hello.txt", "w");
+ * fprintf(f, "Hello World!\n");
+ * fclose(f);
+ * \endcode
+ **************************************************************************************************/
+#define BSP_SPIFFS_MOUNT_POINT      CONFIG_BSP_SPIFFS_MOUNT_POINT
+
+/**
+ * @brief Mount SPIFFS to virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_spiffs_register was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes
+ */
+esp_err_t bsp_spiffs_mount(void);
+
+/**
+ * @brief Unmount SPIFFS from virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_FOUND if the partition table does not contain SPIFFS partition with given label
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_spiffs_unregister was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes
+ */
+esp_err_t bsp_spiffs_unmount(void);
+
+/**************************************************************************************************
+ *
+ * SD card
+ *
+ * After mounting the SD card, it can be accessed with stdio functions ie.:
+ * \code{.c}
+ * FILE* f = fopen(BSP_SD_MOUNT_POINT"/hello.txt", "w");
+ * fprintf(f, "Hello %s!\n", bsp_sdcard->cid.name);
+ * fclose(f);
+ * \endcode
+ *
+ **************************************************************************************************/
+#define BSP_SD_MOUNT_POINT      CONFIG_BSP_SD_MOUNT_POINT
+#define BSP_SDSPI_HOST          (SPI3_HOST)
+
+/**
+ * @brief BSP SD card configuration structure
+ */
+typedef struct {
+    const esp_vfs_fat_sdmmc_mount_config_t *mount;
+    sdmmc_host_t *host;
+    union {
+        const sdmmc_slot_config_t   *sdmmc;
+        const sdspi_device_config_t *sdspi;
+    } slot;
+} bsp_sdcard_cfg_t;
+
+/**
+ * @brief Mount microSD card to virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_sdmmc_mount was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes from SDMMC or SPI drivers, SDMMC protocol, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_mount(void);
+
+/**
+ * @brief Unmount micorSD card from virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_FOUND if the partition table does not contain FATFS partition with given label
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_spiflash_mount was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes from wear levelling library, SPI flash driver, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_unmount(void);
+
+/**
+ * @brief Get SD card handle
+ *
+ * @return SD card handle
+ */
+sdmmc_card_t *bsp_sdcard_get_handle(void);
+
+/**
+ * @brief Get SD card MMC host config
+ *
+ * @param slot SD card slot
+ * @param config Structure which will be filled
+ */
+void bsp_sdcard_get_sdmmc_host(const int slot, sdmmc_host_t *config);
+
+/**
+ * @brief Get SD card SPI host config
+ *
+ * @param slot SD card slot
+ * @param config Structure which will be filled
+ */
+void bsp_sdcard_get_sdspi_host(const int slot, sdmmc_host_t *config);
+
+/**
+ * @brief Get SD card MMC slot config
+ *
+ * @param slot SD card slot
+ * @param config Structure which will be filled
+ */
+void bsp_sdcard_sdmmc_get_slot(const int slot, sdmmc_slot_config_t *config);
+
+/**
+ * @brief Get SD card SPI slot config
+ *
+ * @param spi_host SPI host ID
+ * @param config Structure which will be filled
+ */
+void bsp_sdcard_sdspi_get_slot(const spi_host_device_t spi_host, sdspi_device_config_t *config);
+
+/**
+ * @brief Mount microSD card to virtual file system (MMC mode)
+ *
+ * @param cfg SD card configuration
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_sdmmc_mount was already called
+ *      - ESP_ERR_NO_MEM if memory cannot be allocated
+ *      - ESP_FAIL if partition cannot be mounted
+ *      - other error codes from SDMMC or SPI drivers, SDMMC protocol, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_sdmmc_mount(bsp_sdcard_cfg_t *cfg);
+
+/**
+ * @brief Mount microSD card to virtual file system (SPI mode)
+ *
+ * @param cfg SD card configuration
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_sdmmc_mount was already called
+ *      - ESP_ERR_NO_MEM if memory cannot be allocated
+ *      - ESP_FAIL if partition cannot be mounted
+ *      - other error codes from SDMMC or SPI drivers, SDMMC protocol, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_sdspi_mount(bsp_sdcard_cfg_t *cfg);
+
+/** @} */ // end of storage
+
+/** \addtogroup g04_display
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * LCD interface
+ *
+ * LVGL is used as graphics library. LVGL is NOT thread safe, therefore the user must take LVGL mutex
+ * by calling bsp_display_lock() before calling and LVGL API (lv_...) and then give the mutex with
+ * bsp_display_unlock().
+ *
+ * Display's backlight must be enabled explicitly by calling bsp_display_backlight_on()
+ **************************************************************************************************/
+#define BSP_LCD_PIXEL_CLOCK_HZ     (80 * 1000 * 1000)
+#define BSP_LCD_SPI_NUM            (SPI2_HOST)
+
+#if (BSP_CONFIG_NO_GRAPHIC_LIB == 0)
+
+/**
+ * @brief BSP display configuration structure
+ */
+typedef struct {
+    lvgl_port_cfg_t lvgl_port_cfg;  /*!< LVGL port configuration */
+    uint32_t        buffer_size;    /*!< Size of the buffer for the screen in pixels */
+    bool            double_buffer;  /*!< True, if should be allocated two buffers */
+    struct {
+        unsigned int buff_dma: 1;    /*!< Allocated LVGL buffer will be DMA capable */
+        unsigned int buff_spiram: 1; /*!< Allocated LVGL buffer will be in PSRAM */
+    } flags;
+} bsp_display_cfg_t;
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ * LCD backlight must be enabled separately by calling bsp_display_brightness_set()
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start(void);
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ * LCD backlight must be enabled separately by calling bsp_display_brightness_set()
+ *
+ * @param cfg display configuration
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start_with_config(const bsp_display_cfg_t *cfg);
+
+/**
+ * @brief Get pointer to input device (touch, buttons, ...)
+ *
+ * @note The LVGL input device is initialized in bsp_display_start() function.
+ *
+ * @return Pointer to LVGL input device or NULL when not initialized
+ */
+lv_indev_t *bsp_display_get_input_dev(void);
+
+/**
+ * @brief Take LVGL mutex
+ *
+ * @param timeout_ms Timeout in [ms]. 0 will block indefinitely.
+ * @return true  Mutex was taken
+ * @return false Mutex was NOT taken
+ */
+bool bsp_display_lock(uint32_t timeout_ms);
+
+/**
+ * @brief Give LVGL mutex
+ *
+ */
+void bsp_display_unlock(void);
+
+/**
+ * @brief Set display enter sleep mode
+ *
+ * All the display (LCD, backlight, touch) will enter sleep mode.
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_SUPPORTED if this function is not supported by the panel
+ */
+esp_err_t bsp_display_enter_sleep(void);
+
+/**
+ * @brief Set display exit sleep mode
+ *
+ * All the display (LCD, backlight, touch) will exit sleep mode.
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_SUPPORTED if this function is not supported by the panel
+ */
+esp_err_t bsp_display_exit_sleep(void);
+
+/**
+ * @brief Rotate screen
+ *
+ * Display must be already initialized by calling bsp_display_start()
+ *
+ * @param[in] disp Pointer to LVGL display
+ * @param[in] rotation Angle of the display rotation
+ */
+void bsp_display_rotate(lv_display_t *disp, lv_disp_rotation_t rotation);
+#endif // BSP_CONFIG_NO_GRAPHIC_LIB == 0
+
+/** @} */ // end of display
+
+
+/** @defgroup g08_camera CAMERA
+ *  @brief CAMERA BSP API
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * Camera interface
+ *
+ * TODO: Example configuration:
+ **************************************************************************************************/
+
+#define BSP_CAMERA_XCLK_CLOCK_MHZ     24
+
+/**
+ * @brief Initialize camera
+ *
+ * Camera sensor initialization.
+ */
+esp_err_t bsp_camera_start(void);
+
+/** @} */ // end of adc
+
+
+
+
+/** @defgroup g99_others Others
+ *  @brief Other BSP API
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * BSP Features
+ *
+ * This module provides an interface to enable and disable various features on this board.
+ *
+ * The bsp_feature_enable function is used to enable or disable a specified feature.
+ *
+ * Parameters:
+ * - feature: The feature to enable or disable, of type bsp_feature_t enum.
+ * - enable: A boolean value, true to enable the feature, false to disable it.
+ *
+ * Return value:
+ * - esp_err_t: Error code indicating the result of the operation.
+ *
+ **************************************************************************************************/
+typedef enum {
+    BSP_FEATURE_SD,
+    BSP_FEATURE_LCD,
+    BSP_FEATURE_CAMERA
+} bsp_feature_t;
+
+esp_err_t bsp_feature_enable(bsp_feature_t feature, bool enable);
+
+/** @} */ // end of others
+
+
+/** \addtogroup g06_led
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * LEDs
+ *
+ **************************************************************************************************/
+
+/**
+ * @brief Initialize all LEDs
+ *
+ * @param[out] led_array      Output LED array
+ * @param[out] led_cnt        Number of LED handlers saved to led_array, can be NULL
+ * @param[in]  led_array_size Size of output LED array. Must be at least BSP_LED_NUM
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_indicator_create(led_indicator_handle_t led_array[], int *led_cnt, int led_array_size);
+
+/**
+ * @brief Turn LED on/off
+ *
+ * @param handle led handle
+ * @param on Switch LED on/off
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_set(led_indicator_handle_t handle, const bool on);
+
+/**
+ * @brief Set LED temperature
+ *
+ * @param handle led handle
+ * @param temperature Color temperature of LED
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_set_temperature(led_indicator_handle_t handle, const uint16_t temperature);
+
+/** @} */ // end of leds
+
+
+/** \addtogroup g05_buttons
+ *  @{
+ */
+
+/**************************************************************************************************
+ *
+ * Button
+ *
+ **************************************************************************************************/
+
+/**
+ * @brief Initialize all buttons
+ *
+ * Returned button handlers must be used with espressif/button component API
+ *
+ * @param[out] btn_array      Output button array
+ * @param[out] btn_cnt        Number of button handlers saved to btn_array, can be NULL
+ * @param[in]  btn_array_size Size of output button array. Must be at least BSP_BUTTON_NUM
+ * @return
+ *     - ESP_OK               All buttons initialized
+ *     - ESP_ERR_INVALID_ARG  btn_array is too small or NULL
+ *     - ESP_FAIL             Underlaying iot_button_create failed
+ */
+esp_err_t bsp_iot_button_create(button_handle_t btn_array[], int *btn_cnt, int btn_array_size);
+
+/** @} */ // end of buttons
+
+#ifdef __cplusplus
+}
+#endif
