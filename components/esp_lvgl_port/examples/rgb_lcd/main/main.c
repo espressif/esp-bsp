602ebec710796b812e91a21e84530f45b8e290aa
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-10-29 08:15:54 +0100

    ci(build): Not fail CI, when used IDF from master + cleaning

--- components/esp_lvgl_port/examples/rgb_lcd/main/main.c
+++ components/esp_lvgl_port/examples/rgb_lcd/main/main.c
@@ -254,49 +254,6 @@ static esp_err_t app_lvgl_init(void)
     return ESP_OK;
 }
 
-static void _app_button_cb(lv_event_t *e)
-{
-    lv_disp_rotation_t rotation = lv_disp_get_rotation(lvgl_disp);
-    rotation++;
-    if (rotation > LV_DISPLAY_ROTATION_270) {
-        rotation = LV_DISPLAY_ROTATION_0;
-    }
-
-    /* LCD HW rotation */
-    lv_disp_set_rotation(lvgl_disp, rotation);
-}
-
-static void app_main_display(void)
-{
-    lv_obj_t *scr = lv_scr_act();
-
-    /* Your LVGL objects code here .... */
-
-    /* Create image */
-    lv_obj_t *img_logo = lv_img_create(scr);
-    lv_img_set_src(img_logo, &esp_logo);
-    lv_obj_align(img_logo, LV_ALIGN_TOP_MID, 0, 20);
-
-    /* Label */
-    lv_obj_t *label = lv_label_create(scr);
-    lv_obj_set_width(label, EXAMPLE_LCD_H_RES);
-    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
-#if LVGL_VERSION_MAJOR == 8
-    lv_label_set_recolor(label, true);
-    lv_label_set_text(label, "#FF0000 "LV_SYMBOL_BELL" Hello world Espressif and LVGL "LV_SYMBOL_BELL"#\n#FF9400 "LV_SYMBOL_WARNING" For simplier initialization, use BSP "LV_SYMBOL_WARNING" #");
-#else
-    lv_label_set_text(label, LV_SYMBOL_BELL" Hello world Espressif and LVGL "LV_SYMBOL_BELL"\n "LV_SYMBOL_WARNING" For simplier initialization, use BSP "LV_SYMBOL_WARNING);
-#endif
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 20);
-
-    /* Button */
-    lv_obj_t *btn = lv_btn_create(scr);
-    label = lv_label_create(btn);
-    lv_label_set_text_static(label, "Rotate screen");
-    lv_obj_align(btn, LV_ALIGN_BOTTOM_MID, 0, -30);
-    lv_obj_add_event_cb(btn, _app_button_cb, LV_EVENT_CLICKED, NULL);
-}
-
 void app_main(void)
 {
     /* LCD HW initialization */
@@ -310,7 +267,7 @@ void app_main(void)
 
     /* Show LVGL objects */
     lvgl_port_lock(0);
-    //app_main_display();
     lv_demo_music();
     lvgl_port_unlock();
+
 }
