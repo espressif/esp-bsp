8ed743e527f2a5901046b5aed7fd61aaee0ad717
Tomas Rezucha <tomas.rezucha@espressif.com>
2026-04-10 15:01:17 +0200

    feat(wrover_kit): Add noglib menuconfig option

--- bsp/esp_wrover_kit/include/bsp/config.h
+++ bsp/esp_wrover_kit/include/bsp/config.h
@@ -1,16 +1,19 @@
 /*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2024-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
+#include "sdkconfig.h"
+
 /**************************************************************************************************
  * BSP configuration
  **************************************************************************************************/
-// By default, this BSP is shipped with LVGL graphical library. Enabling this option will exclude it.
-// If you want to use BSP without LVGL, select BSP version with 'noglib' suffix.
-#if !defined(BSP_CONFIG_NO_GRAPHIC_LIB) // Check if the symbol is not coming from compiler definitions (-D...)
+/* LVGL integration: default on; disable via menuconfig (CONFIG_BSP_LVGL_INTEGRATION). */
+#if CONFIG_BSP_LVGL_INTEGRATION
 #define BSP_CONFIG_NO_GRAPHIC_LIB (0)
+#else
+#define BSP_CONFIG_NO_GRAPHIC_LIB (1)
 #endif
