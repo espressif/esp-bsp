d6098bef660f170a39bdc531dc78e945351b8f45
Petr Opravil <petr.opravil@espressif.com>
2026-04-15 13:26:06 +0200

    fix(ci): Add missing compilation tests and update component build rules

---
+++ components/lcd_touch/esp_lcd_touch_ft5x06/test_apps/main/compilation_test.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2024-2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_lcd_touch_ft5x06.h"
+
+void app_main(void)
+{
+    // Just enough to test component compilation
+}
