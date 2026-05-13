cd246d1418b5f088145fd52069dab406e9c05a1b
Vilem Zavodny <vilem.zavodny@espressif.com>
2026-05-13 09:58:39 +0200

    ci(BSP Wall): Add ESP VoCat and M5Stack Tab5 to BSP Wall

--- examples/display_lvgl_demos/pytest_display_lvgl_demos.py
+++ examples/display_lvgl_demos/pytest_display_lvgl_demos.py
@@ -13,6 +13,8 @@ from pytest_embedded import Dut
 @pytest.mark.m5dial
 @pytest.mark.m5stack_core_s3
 @pytest.mark.m5stack_core_s3_se
+@pytest.mark.esp_vocat
+@pytest.mark.m5stack_tab5
 def test_example_lvgl_demos(dut: Dut) -> None:
     dut.expect_exact('app_main: Display LVGL demo')
     dut.expect_exact('main_task: Returned from app_main()')
